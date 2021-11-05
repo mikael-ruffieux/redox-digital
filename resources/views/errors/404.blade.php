47ed6a8eab8cb7a478b4fbc4dfbae144de98eb6d
Mikaël Ruffieux <ruffieux.mikael@gmail.com>
2021-11-05 20:58:18 +0100

    v.2.1.3
    Adding custom error pages (404, 500, 503)

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.error', [
+    'page_title' => 'Page introuvable',
+    'page_description' => 'La page que vous recherchez a peut-être été renommée, supprimé ou n\'existe peut-être tout simplement pas.',
+    'errorCode' => '404',
+])
