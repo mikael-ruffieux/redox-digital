29bc48d460bda5f5d09d9447b1f145cb0eb7778f
Mikaël Ruffieux <ruffieux.mikael@gmail.com>
2021-12-04 14:56:49 +0100

    Update UsersTableSeeder.php

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -21,19 +21,19 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->insert([
             'name' => 'admin',
             'email' => 'info@redoxdigital.ch',
-            'password' => Hash::make('MiRu6618$'), // not the production password
+            'password' => Hash::make('12345678'), // not the production password
         ]);
 
         DB::table('users')->insert([
             'name' => 'mikael',
             'email' => 'mikael@redoxdigital.ch',
-            'password' => Hash::make('MiRu6618$'), // not the production password
+            'password' => Hash::make('12345678'), // not the production password
         ]);
 
         DB::table('users')->insert([
             'name' => 'etienne',
             'email' => 'etienne@redoxdigital.ch',
-            'password' => Hash::make('EtWi0564$'), // not the production password
+            'password' => Hash::make('12345678'), // not the production password
         ]);
     }
 }
